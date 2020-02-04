bdf935e9854c0d92824d6d6b47f164b389ca2b6b
Maurice <mauricederidder@outlook.com>
2020-02-04 15:49:30 +0100

    added auth and bootstrap

--- blog/resources/views/products/index.blade.php
+++ blog/resources/views/products/index.blade.php
@@ -1,10 +1,11 @@
 @extends('templates.app')
 
 @section('content')
-    <ol>
-        @foreach ($items as $item)
-            <li><h2>{{$item->name}}</h2><p>{{$item->description}}</p></li>
-        @endforeach
-    </ol>
-
+    <div class="row">
+        <ol>
+            @foreach ($items as $item)
+                <li class="item col-md-4"><h2>{{$item->name}}</h2><p>{{$item->description}}</p></li>
+            @endforeach
+        </ol>
+    </div>
 @stop
--- blog/resources/views/templates/app.blade.php
+++ blog/resources/views/templates/app.blade.php
@@ -4,13 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     <title></title>
-</head>
+    <style>
+        /* #GENERAL PAGE                  */
+        body{
+            background-color: grey;
+        }
+        /* #NAV BAR                       */
+
+        /* #PRODUCTS PAGE                 */
+        .item{
+            background-color:white;
+            width:100px;
+            height:100px;
+        }
+        .item li{
+            list-style: none;
+        }
+    </style>
+ </head>
 <body>
     @include('templates.nav')
 
     <div class="container">
         @yield('content')
     </div>
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="{{ asset('js/bootstrap.min.js') }}"></script>
 </body>
 </html>
